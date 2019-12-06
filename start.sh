357e4f5b0684e89882c687a77a4e32fd74ced3aa
Feihong Hsu <feihong.hsu@gmail.com>
2019-12-06 09:32:38 +0800

    Print random hanzi

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker run --rm \
+      -p 8888:8888 \
+      -v $PWD:/home/jovyan/pwd \
+      --env JUPYTER_ENABLE_LAB=yes \
+      --env JUPYTER_TOKEN=x \
+      --name ihaskell_notebook \
+      crosscompass/ihaskell-notebook:latest
